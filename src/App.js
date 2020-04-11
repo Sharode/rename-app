2572884c6d65592d679adce1e0dbd542d50ef210
Neil Williams <Williams.Sharode@gmail.com>
2020-04-11 12:31:27 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="text-blue-800">Hello world</header>
+		</div>
+	);
+}
+
+export default App;
